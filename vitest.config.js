afe1b6b5863db817360ac5916db4b20124d8c708
Rais <aksu3796@gmail.com>
2025-01-27 19:51:07 +0300

    Router problem fix4

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: '/', // Bu satırı ekleyin
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
